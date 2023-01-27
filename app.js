efd260003d14d1572b26de04f3b44ecbd82ee9b5
Elis Mora <almedinaelis.mora@gmail.com>
2023-01-27 18:05:11 +0100

    inicializando la app. app.js modificado con las rutas necesitadas

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const addbookRouter = require('./routes/addbook')
+const addbookRouter = require('./routes/addbook');
+const booksRouter = require('./routes/books')
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/addbook', addbookRouter);
+app.use('/books', booksRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
