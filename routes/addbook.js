a464d10d36f68a35f52a33d684187aad369d119b
Elis Mora <almedinaelis.mora@gmail.com>
2023-01-27 18:10:33 +0100

    ruta y vista para añadir libros avanzada

--- routes/addbook.js
+++ routes/addbook.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const biblio = require('../biblio.json')
 
 router.get('/', function(req, res, next) {
     res.render('newbook', { title: 'Add a New Book' });
   });
+
+
+router.post('/', function(req, res, next) {
+    // console.log(req.body)
+    // res.send('libro agregado')
+
+    const { title, author, description } = req.body
+
+    if ( req.body != null ){
+      
+      biblio.push(req.body)
+      console.log(biblio)
+    }
+    
+
+
+    res.redirect('/books')
+});
+
 
 
 module.exports = router;
