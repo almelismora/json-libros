0250b3befd7e3d0a429b78b5e0b05e48961bfbe6
Elis Mora <almedinaelis.mora@gmail.com>
2023-01-27 18:32:35 +0100

    fotos de libros agregados en la pagina de inicio

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const biblio = require('../biblio.json')
+
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressBooks' });
+  res.render('index', { title: 'ExpressBooks', biblio: biblio });
+
 });
 
 
